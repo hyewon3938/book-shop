75893be0784597c87feb52dffa76bbe45c690453
Joy <hyewon3938@naver.com>
2021-07-09 02:13:59 +0900

    [#2] chore : 웹팩 환경설정
    - 웹팩 환경설정 파일 추가

---
+++ client/webpack.prod.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "production",
+  output: {
+    filename: "[name].bundle.js",
+    chunkFilename: "[name].bundle.js",
+    path: path.resolve(__dirname, "./build"),
+    publicPath: "/",
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  plugins: [
+    new Dotenv({
+      path: path.resolve(__dirname, "./.env.production"),
+    }),
+  ],
+});
