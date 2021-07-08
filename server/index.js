69177259e3baf4e7704922625a802d6fad2a9109
Joy <hyewon3938@naver.com>
2021-07-09 01:32:52 +0900

    [#1] chore : Mongo DB 연결
    - mongo DB 연결
    - mongo DB URL 개발모드, 상품모드에 따라 url 분기처리

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./config/key.js");
 
 const app = express();
 const port = 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("hello world!");
