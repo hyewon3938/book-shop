db9e1a5416d2b7937d84ae52e0ac4e6a41d0719d
Joy <hyewon3938@naver.com>
2021-07-10 05:01:38 +0900

    m,.

--- server/config/db.js
+++ server/config/db.js
@@ -12,7 +12,7 @@ const connectDB = async () => {
 
     console.log("MongoDB connection SUCCESS");
   } catch (error) {
-    console.error(`MongoDB connection FAIL!! ${process.env.MONGO_URI} `);
+    console.error(`MongoDB connection FAIL`);
     process.exit(1);
   }
 };
