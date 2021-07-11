0c1c0d8eefc76985eac3e51711850a96c10b777c
Joy <hyewon3938@naver.com>
2021-07-12 05:37:25 +0900

    [#9] feat : 메뉴 클릭 여부 상태값 관리 코드 추가
    - 메뉴 클릭 여부 상태값 관리 코드 추가

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 // Components
 import Navbar from "@/components/Header/Navbar";
+import BackDrop from "@/components/Header/BackDrop";
+import SideDrawer from "@/components/Header/SideDrawer";
 
 const Header = () => {
+  const [sideToggle, setSideToggle] = useState(false);
+
   return (
     <>
-      <Navbar />
+      <Navbar click={() => setSideToggle(true)} />
+      <SideDrawer show={sideToggle} click={() => setSideToggle(false)} />
+      <BackDrop show={sideToggle} click={() => setSideToggle(false)} />
     </>
   );
 };
