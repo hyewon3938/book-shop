5cdb42b5b9bdad3ce0fbf6d236868a646ea54ef3
Joy <hyewon3938@naver.com>
2021-07-12 01:23:39 +0900

    fix : server/index.js 의 경로 코드 수정
    - server/index.js 의 경로 코드 수정

--- server/index.js
+++ server/index.js
@@ -22,5 +22,5 @@ app.listen(PORT, () => {
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "../client/build/index.html"));
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
