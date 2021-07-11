f58975ea101ee7c274fe9d68f9a8aac50dcc42cb
Joy <hyewon3938@naver.com>
2021-07-12 00:14:51 +0900

    [#8] feat : 컴포넌트 라우팅 설정
    - 컴포넌트 라우팅 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Components
 import GlobalStyleProvider from "@/components/style/GlobalStyleProvider";
+import Header from "@/components/Header/Header";
+import HomePage from "@/components/HomePage/HomePage";
+import ProductsPage from "@/components/ProductsPage/ProductsPage";
+import ProductDetailsPage from "@/components/ProductDetailsPage/ProductDetailsPage";
+import CartPage from "@/components/CartPage/CartPage";
 
 const App = () => {
   return (
     <GlobalStyleProvider>
-      <div>
-      </div>
+      <Router>
+        <Header />
+        <main>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/product/:category" component={ProductsPage} />
+            <Route exact path="/product/:category/:id" component={ProductDetailsPage} />
+            <Route exact path="/cart" component={CartPage} />
+          </Switch>
+        </main>
+      </Router>
     </GlobalStyleProvider>
   );
 };
