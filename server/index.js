30b681940ae9352fe0c8ca1934793ea5d0c4641d
Joy <hyewon3938@naver.com>
2021-07-12 01:08:32 +0900

    fix : 헤로쿠 react-router 에러 문제
    - 아직 미해결, 코드 원상 복구

--- server/index.js
+++ server/index.js
@@ -21,18 +21,6 @@ app.listen(PORT, () => {
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-let protected = ["transformed.js"];
-
 app.get("*", (req, res) => {
-  // res.sendFile(path.join(__dirname + "../client/build/index.html"));
-
-  let path = req.params["0"].substring(1);
-
-  if (protected.includes(path)) {
-    // Return the actual file
-    res.sendFile(`${__dirname}/build/${path}`);
-  } else {
-    // Otherwise, redirect to /build/index.html
-    res.sendFile(`${__dirname}/build/index.html`);
-  }
+  res.sendFile(path.join(__dirname + "../client/build/index.html"));
 });
