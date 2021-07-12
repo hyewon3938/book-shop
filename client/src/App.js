0a40c3de631a2965996a894a7a7cecb5873aea39
Joy <hyewon3938@naver.com>
2021-07-12 14:10:18 +0900

    [#8] fix : 서버에서 url 접근 에러 해결
    - browserRouter 대신 hashRouter 사용

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 // Components
 import GlobalStyleProvider from "@/components/style/GlobalStyleProvider";
