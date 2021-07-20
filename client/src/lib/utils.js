2cb40eff76423e576d99839bdd5c2696e48c0224
Joy <hyewon3938@naver.com>
2021-07-20 17:59:39 +0900

    [#15] feat : util 함수에 스크롤 상단으로 이동하는 기능 추가
    - util 함수에 scrollToTop 함수 추가

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -2,3 +2,7 @@ export const numberWithCommas = (numberValue) => {
   const number = numberValue.toString().replace(/,/gi, "");
   return Number(number).toLocaleString();
 };
+
+export const scrollToTop = () => {
+  window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+};
