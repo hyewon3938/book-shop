968c0250cb337102eccdfa41c28342fb3a21918b
Joy <hyewon3938@naver.com>
2021-07-21 23:36:12 +0900

    [#16] feat : style 폴더에 공통으로 적용될 skeleton loading animation 파일 추가
    - style 폴더에 공통으로 사용될 animation 추가

---
+++ client/src/components/style/skeletonLoadingAnimation.jsx
@@ -0,0 +1,13 @@
+import { keyframes } from "styled-components";
+
+export const shine = keyframes`
+  from {
+    opacity : 0.4;
+  }
+  50% {
+    opacity : 0.7;
+  }
+  to {
+    opacity  : 0.4;
+  }
+`;
