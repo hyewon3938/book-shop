988b00d28d4c5f2094b7a8626e3f600e2e640133
Joy <hyewon3938@naver.com>
2021-07-22 20:30:18 +0900

    [#18] feat : 카트 전체 삭제 Action 함수 추가
    - 카트 전체 삭제 Action 함수 추가

--- client/src/redux/actions/cartActions.js
+++ client/src/redux/actions/cartActions.js
@@ -28,3 +28,10 @@ export const removeFromCart = (id) => (dispatch, getState) => {
 
   localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems));
 };
+
+export const removeAllCart = () => (dispatch) => {
+  dispatch({
+    type: actionTypes.CART_RESET,
+  });
+  localStorage.removeItem("cart");
+};
