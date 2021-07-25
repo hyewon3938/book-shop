7757b8b12016124c4a3f15e3d4f4bd6e3038fd3b
Joy <hyewon3938@naver.com>
2021-07-26 06:47:26 +0900

    refactor : 불필요한 컴포넌트 삭제
    - 불필요한 컴포넌트 삭제

--- client/src/components/HomePage/NewProductList.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NewProductList = () => {
-  return <div>NewProductList</div>;
-};
-
-export default NewProductList;
--- client/src/components/HomePage/ServiceDescription.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ServiceDescription = () => {
-  return <div>ServiceDescription</div>;
-};
-
-export default ServiceDescription;
