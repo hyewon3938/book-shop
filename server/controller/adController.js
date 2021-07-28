9463f0a3647ee80d9dd4afec7882d769b6da01a8
Joy <hyewon3938@naver.com>
2021-07-29 05:44:22 +0900

    [#24] fix: adController 첫번째 데이터만 response하도록 변경
    - 하나의 데이터만 필요하므로 첫번째 데이터만  response 하도록 변경

--- server/controller/adController.js
+++ server/controller/adController.js
@@ -4,7 +4,7 @@ const getAds = async (req, res) => {
   try {
     const ads = await Ad.find({});
 
-    res.json(ads);
+    res.status(200).json(ads[0]);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server Error" });
