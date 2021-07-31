bdd63b96208965f1e96f03e66d5d489aade1126f
Joy <hyewon3938@naver.com>
2021-08-01 05:41:45 +0900

    [#28] feat : userSchema method에 비밀번호 비교 함수 추가
    - req의 비밀번호를 암호화된 비밀번호와 비교

--- server/models/User.js
+++ server/models/User.js
@@ -56,6 +56,13 @@ userSchema.pre("save", function (next) {
   }
 });
 
+userSchema.methods.comparePassword = function (plainPassword, callback) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return callback(err);
+    callback(null, isMatch);
+  });
+};
+
 const User = mongoose.model("user", userSchema);
 
 module.exports = User;
