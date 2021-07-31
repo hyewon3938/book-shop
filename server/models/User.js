79be193fbe478dfa0a1cc1d07b24e938ff64be20
Joy <hyewon3938@naver.com>
2021-08-01 05:42:20 +0900

    [#28] feat : userSchema method에 jwt 토큰 발행 함수 추가
    - jwt 토큰 발행 함수 추가

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -60,6 +61,18 @@ userSchema.methods.comparePassword = function (plainPassword, callback) {
   bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
     if (err) return callback(err);
     callback(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (callback) {
+  let user = this;
+
+  let token = jwt.sign(user._id.toHexString(), "secretToken");
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return callback(err);
+    callback(null, user);
   });
 };
 
