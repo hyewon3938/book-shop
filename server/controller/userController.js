60ec7a5f6318323a55b95baa11259b7cbfd8e58b
Joy <hyewon3938@naver.com>
2021-08-01 05:39:41 +0900

    [#26,#27] refactor : 불필요한 에러처리 코드 제거
    - 불필요한 에러처리 코드 제거

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -4,32 +4,22 @@ const postUserInfo = (req, res) => {
   const user = new User(req.body);
 
   user.save((err, userInfo) => {
-    try {
-      if (err) {
-        console.log(err);
-        res.json({ success: false, err });
-        return;
-      }
-      res.status(200).json({
-        success: true,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Server Error" });
+    if (err) {
+      console.log(err);
+      res.json({ success: false, err });
+      return;
     }
+    res.status(200).json({
+      success: true,
+    });
   });
 };
 
 const emailDuplicateCheck = async (req, res) => {
-  try {
-    const email = await User.findOne({ email: req.body.email });
-    if (email) return res.status(409).json({ isAvailable: false });
+  const email = await User.findOne({ email: req.body.email });
+  if (email) return res.status(409).json({ isAvailable: false });
 
-    res.status(200).json({ isAvailable: true });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server Error" });
-  }
+  res.status(200).json({ isAvailable: true });
 };
 
 module.exports = {
