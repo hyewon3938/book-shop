3da6227f5cbb628e58fcb1d884d18fa8a8e368e1
Joy <hyewon3938@naver.com>
2021-08-01 04:30:18 +0900

    [#27] feat : userRoutes에 /check-email 라우터 등록
    - userRoutes에 /check-email 라우터 등록

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { postUserInfo } = require("../controller/userController");
+const { postUserInfo, emailDuplicateCheck } = require("../controller/userController");
 
 // @desc POST userInfo to db
 // @route POST /api/users/register
 // @access Public
 router.post("/register", postUserInfo);
+
+// @desc POST email to check
+// @route POST /api/users/check-email
+// @access Public
+router.post("/check-email", emailDuplicateCheck);
 
 module.exports = router;
