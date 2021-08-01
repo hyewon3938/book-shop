af77174fdbbf8b60374065a14d23bc1887af1693
Joy <hyewon3938@naver.com>
2021-08-02 01:59:21 +0900

    [#28,#29] refactor : 로그인 로그아웃 controller 함수 userController 파일로 이동
    - 로그인 로그아웃 controller 함수 userController 파일로 이동

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -22,6 +22,27 @@ const emailDuplicateCheck = async (req, res) => {
   res.status(200).json({ isAvailable: true });
 };
 
+const login = (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "가입된 이메일이 없습니다.",
+      });
+    }
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) return res.json({ loginSuccess: false, message: "잘못된 비밀번호입니다." });
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        res.cookie("x_auth", user.token).status(200).json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+};
+
 const auth = (req, res) => {
   res.status(200).json({
     _id: req.user._id,
@@ -32,8 +53,20 @@ const auth = (req, res) => {
     points: req.user.points,
   });
 };
+
+const logout = (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
+  });
+};
+
 module.exports = {
   postUserInfo,
   emailDuplicateCheck,
+  login,
+  logout,
   auth,
 };
