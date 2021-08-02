c9f52e15c48b3a51661ea3387235e4058b55173a
Joy <hyewon3938@naver.com>
2021-08-02 12:29:02 +0900

    [#33] feat : 쿠키값을 지우는 함수 추가
    - 쿠키값을 지우는 함수 추가

--- client/src/lib/cookies.js
+++ client/src/lib/cookies.js
@@ -2,3 +2,7 @@ export const getCookie = (cookieName) => {
   var value = document.cookie.match("(^|;) ?" + cookieName + "=([^;]*)(;|$)");
   return value ? value[2] : null;
 };
+
+export const deleteCookie = (cookieName) => {
+  document.cookie = cookieName + "=; expires=Thu, 01 Jan 1999 00:00:10 GMT;";
+};
