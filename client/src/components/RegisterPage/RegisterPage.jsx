7c1740fdc370ea6673c33f6bedd3ffa5586cb97a
Joy <hyewon3938@naver.com>
2021-08-03 08:11:00 +0900

    [#35] fix : 이름, 이메일 조건검사 로직 수정
    - length로 비교하지 않고 boolean 값으로 비교

--- client/src/components/RegisterPage/RegisterPage.jsx
+++ client/src/components/RegisterPage/RegisterPage.jsx
@@ -38,8 +38,7 @@ const RegisterPage = () => {
     const emailCheck =
       /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
 
-    if (!removeBlank(target.value).length)
-      return fail(target, emailMessage, "필수 입력 항목입니다.");
+    if (!removeBlank(target.value)) return fail(target, emailMessage, "필수 입력 항목입니다.");
     if (!emailCheck.test(target.value))
       return fail(target, emailMessage, "이메일을 다시 확인해주세요.");
 
@@ -49,8 +48,7 @@ const RegisterPage = () => {
 
   const checkName = () => {
     const target = name.current;
-    if (!removeBlank(target.value).length)
-      return fail(target, nameMessage, "필수 입력 항목입니다.");
+    if (!removeBlank(target.value)) return fail(target, nameMessage, "필수 입력 항목입니다.");
     pass(target, nameMessage);
   };
 
@@ -76,8 +74,7 @@ const RegisterPage = () => {
     history.push("/");
   };
 
-  const registerClickHandler = (e) => {
-    e.preventDefault();
+  const registerClickHandler = () => {
     checkEmail();
     checkName();
     checkPassword();
@@ -91,33 +88,31 @@ const RegisterPage = () => {
       </Logo>
       <Title>회원가입</Title>
       <RegisterWrap>
-        <Form onSubmit={registerClickHandler}>
-          <InputWrap>
-            <Input type="email" placeholder="이메일" onBlur={checkEmail} ref={email} />
-            <Message ref={emailMessage}></Message>
-            <Input placeholder="이름" onBlur={checkName} ref={name} />
-            <Message ref={nameMessage}></Message>
-            <Input
-              type="password"
-              placeholder="비밀번호 (8~16자 영문,숫자)"
-              ref={password}
-              onBlur={checkPassword}
-            />
-            <Message ref={passwordMessage}></Message>
-            <Input
-              type="password"
-              placeholder="비밀번호 확인"
-              ref={confirmPassword}
-              onBlur={checkConfirmPassword}
-            />
-            <Message ref={confirmPasswordMessage}></Message>
-            {!loading ? <LoadingIndicator /> : ""}
-          </InputWrap>
-          <ButtonWrap>
-            {!loading ? <LoadingIndicator /> : ""}
-            <Button type="submit">회원가입</Button>
-          </ButtonWrap>
-        </Form>
+        <InputWrap>
+          <Input type="email" placeholder="이메일" onBlur={checkEmail} ref={email} />
+          <Message ref={emailMessage}></Message>
+          <Input placeholder="이름" onBlur={checkName} ref={name} />
+          <Message ref={nameMessage}></Message>
+          <Input
+            type="password"
+            placeholder="비밀번호 (8~16자 영문,숫자)"
+            ref={password}
+            onBlur={checkPassword}
+          />
+          <Message ref={passwordMessage}></Message>
+          <Input
+            type="password"
+            placeholder="비밀번호 확인"
+            ref={confirmPassword}
+            onBlur={checkConfirmPassword}
+          />
+          <Message ref={confirmPasswordMessage}></Message>
+          {!loading ? <LoadingIndicator /> : ""}
+        </InputWrap>
+        <ButtonWrap>
+          {!loading ? <LoadingIndicator /> : ""}
+          <Button onClick={registerClickHandler}>회원가입</Button>
+        </ButtonWrap>
       </RegisterWrap>
     </Wrap>
   );
@@ -135,10 +130,10 @@ const Wrap = styled.div`
   background: #f5f5ef;
   display: flex;
   flex-direction: column;
-  /* justify-content: center; */
   align-items: center;
   z-index: 10000;
   overflow-y: scroll;
+  overflow-x: hidden;
 `;
 
 const Logo = styled.div`
