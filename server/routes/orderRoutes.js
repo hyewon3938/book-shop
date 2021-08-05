de4d9567490818aa0d98e90756718eff91dc4102
Joy <hyewon3938@naver.com>
2021-08-06 08:49:12 +0900

    [#41] feat : 상품 주문내역 가져오기 API 라우트 연결
     - 상품 주문내역 가져오기 API 라우트 연결

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { pay, addOrder, checkCountOfStock } = require("../controller/orderController");
+const { pay, addOrder, checkCountOfStock, getOrderList } = require("../controller/orderController");
 
 // @desc POST orderInfo to db
 // @route POST /api/orders
@@ -12,5 +12,10 @@ router.post("/", pay, addOrder);
 // @route POST /api/orders/check-stock
 // @access Public
 router.post("/check-stock", checkCountOfStock);
+
+// @desc GET orderList from db
+// @route GET /api/orders/:userId
+// @access Public
+router.get("/:userId", getOrderList);
 
 module.exports = router;
