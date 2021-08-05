a464c515f300eadce1cf1d79c9b19361a0f33354
Joy <hyewon3938@naver.com>
2021-08-06 05:11:03 +0900

    [#40] feat : 재고확인 api 라우트 생성
     - 재고확인 api 라우트 생성

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { pay, addOrder } = require("../controller/orderController");
+const { pay, addOrder, checkCountOfStock } = require("../controller/orderController");
 
-// @desc POST userInfo to db
+// @desc POST orderInfo to db
 // @route POST /api/orders
 // @access Public
 router.post("/", pay, addOrder);
+
+// @desc POST productList to check count of stock
+// @route POST /api/orders/check-stock
+// @access Public
+router.post("/check-stock", checkCountOfStock);
 
 module.exports = router;
