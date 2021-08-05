1df7bb6f4e3ec8c5a5cbba1aaf0e0164e572832e
Joy <hyewon3938@naver.com>
2021-08-05 12:17:49 +0900

    [#39] feat : Order 모델 스키마 추가
    - Order 모델 스키마 추가

---
+++ server/models/Order.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    require: true,
+  },
+  productList: [
+    {
+      productId: {
+        type: String,
+        require: true,
+      },
+      numberOfOrder: {
+        type: Number,
+        require: true,
+      },
+    },
+  ],
+  orderDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  paymentType: {
+    type: String,
+    require: true,
+    default: "적립금",
+  },
+  totalPayment: {
+    type: Number,
+    require: true,
+  },
+  orderStatus: {
+    type: String,
+    require: true,
+    default: "결제완료",
+  },
+});
+
+const Order = mongoose.model("order", orderSchema);
+
+module.exports = Order;
