96e442c56f1e1c6510ebe0973b67273ee4645b54
hyewon3938 <hyewon3938@naver.com>
2023-03-02 20:55:58 +0900

    fix : 웹팩 dev 모드 분기처리 로직 수정

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,4 @@
 module.exports = function (env) {
-  console.log(env?.dev);
-  if (env?.dev) return require(`./webpack.dev.js`);
+  if (env.dev) return require(`./webpack.dev.js`);
   return require(`./webpack.prod.js`);
 };
